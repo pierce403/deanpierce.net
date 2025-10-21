1e414d46d015ede59fe2a70c029982088232a347
pierce403 <pierce403@users.noreply.github.com>
2025-10-21 13:19:41 +0000

    deploy: cafc1d80df3e7260d8ba806112b9e97ced51e23b

--- js/expand-nav.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggles = document.querySelectorAll('.md-nav__toggle');
-  for (const toggle of toggles) {
-    toggle.checked = true; // keep all navigation branches expanded
-  }
-});
